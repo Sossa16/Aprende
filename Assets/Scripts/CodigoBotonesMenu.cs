f1d8bb07a9cbba7b595c54f8900c34f4f7d5d98b
Cardozo <cardozoqsantiago@gmail.com>
2018-04-08 18:51:56 -0500

    Menu funcional

--- Assets/Scripts/CodigoBotonesMenu.cs
+++ Assets/Scripts/CodigoBotonesMenu.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class EventoBotonEstu : MonoBehaviour {
+public class CodigoBotonesMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +14,8 @@ public class EventoBotonEstu : MonoBehaviour {
 	void Update () {
 		
 	}
+	public void cargarEscenas(string pnombreescena){
+		SceneManager.LoadScene (pnombreescena);
+	}
+
 }
