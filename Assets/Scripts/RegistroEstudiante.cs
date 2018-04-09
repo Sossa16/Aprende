a124d7c6e27f0e7ddf25126e1d184d6097c7810a
Santiago <santiago_1016@hotmail.com>
2018-04-09 17:06:59 -0500

    primera version registro

---
+++ Assets/Scripts/RegistroEstudiante.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegistroEstudiante : MonoBehaviour {
+
+	public InputField name;
+	public InputField id;
+
+	public void guardar(){
+		string idProfesor = id.text;
+		string nombre = name.text;
+		PlayerPrefs.SetString ("nombre", nombre);
+		PlayerPrefs.SetString ("idProfesor", idProfesor);
+		print (nombre + " " + idProfesor);
+	}
+
+	public void imprimir(){
+		PlayerPrefs.GetString ("nombre");
+	}
+}
