66591aa31acffde985114a22866ea045de5c4bf0
Bedoya <santiago_1016@hotmail.com>
2018-05-28 15:02:45 -0500

    Arreglo preguntas

--- Assets/Scripts/CambioPregunta.cs
+++ Assets/Scripts/CambioPregunta.cs
@@ -6,6 +6,30 @@ public class CambioPregunta : MonoBehaviour {
 
 	public void Cambio()
 	{
+		if (ControladorTexto.Seleccionada == "y") {
+			//numeroPregunta += 1;
+			ControladorTexto.Seleccionada = "n";
+
+			print (ControladorTexto.respuestas [ControladorTexto.preguntaRandom]+ " " + ControladorTexto.RespuestaSeleccionada);
+			if (ControladorTexto.respuestas [ControladorTexto.preguntaRandom] == ControladorTexto.RespuestaSeleccionada) {
+
+				ControladorTexto.totalPreguntas = ControladorTexto.totalPreguntas + 1;
+				ControladorTexto.preguntasCorrectas = ControladorTexto.preguntasCorrectas + 1;
+				print ("si");
+				if (ControladorTexto.totalPreguntas==3) {
+					Reporte.OnMouseDown ();
+				}
+
+			} else {
+
+				ControladorTexto.totalPreguntas = ControladorTexto.totalPreguntas +1;
+
+				if(ControladorTexto.totalPreguntas ==3){
+					Reporte.OnMouseDown ();
+				}
+			}
+
+		}
 		ControladorTexto.preguntaRandom = -1;
 
 	}
